18c3dfdb597a8d5299a2b4a6801f40afe4d89e57
drougard <drougardn@gmail.com>
2016-01-18 03:26:20 +0100

    remove person first step

--- client/components/requestCreation/requestRemovePerson/requestRemovePerson.controller.js
+++ client/components/requestCreation/requestRemovePerson/requestRemovePerson.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('styleyApp')
-  .controller('RequestRemovePersonCtrl', function ($scope) {
+  .controller('RequestRemovePersonCtrl', function ($scope,) {
     $scope.message = 'Hello';
   })
-    .controller('CreaInstanceCtrl', function ($scope) {
+    .controller('CreaInstanceCtrl', function ($scope,$timeout) {
+	$scope.step=1;
+	$scope.sliderSrc1 = '../../../assets/images/rem1.png'
+  $scope.sliderSrc2 = '../../../assets/images/rem2.png'	
+  $timeout(function(){$('.slider').slider(); }, 100);
+	$scope.nextStep = function(){$scope.step++};
 	})
