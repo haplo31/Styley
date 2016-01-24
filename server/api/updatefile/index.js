bdfd9027d0c25b3758b8ac93a50d99335ce0675c
John <haplo31@neuf.fr>
2016-01-24 23:22:15 +0100

    resize updated pictures / originals in uploads/qqrequest/originals and resized pictures on uploads/qqrequest/samples

--- server/api/updatefile/index.js
+++ server/api/updatefile/index.js
@@ -1,5 +1,6 @@
 'use strict';
 import multer from 'multer';
+import sharp from 'sharp'
 var express = require('express');
 var app= require ("../../app.js")
 var router = express.Router();
@@ -10,44 +11,15 @@ var busboy = require('connect-busboy');
 var fs = require('fs-extra');
 //var lwip=require('lwip');
 var upload = multer({ //multer settings
-	dest: __dirname +'./../../uploads/'
+	dest: __dirname +'./../../uploads/qqrequest/originals'
 });
 router.post('/',upload.single('file'), function (req, res) {
-	res.json({
-    filename: req.file.filename
-  });
-  // req.pipe(req.busboy);
-  // req.busboy.on('file', function (fieldname, file, filename) {
-  //   var stream = fs.createWriteStream(__dirname + '/uploads/' + filename);
-  //   file.pipe(stream);
-  //   stream.on('close', function () {
-	 //    console.log('File ' + filename + ' is uploaded');
-	 //    //USE EASYIMAGE module//
-		// 	// lwip.open(__dirname +'/uploads/'+filename, function(err, image){
-		// 	//   // check err...
-		// 	//   // define a batch of manipulations and save to disk as JPEG:
-		// 	//   var imgRatio = image.height()/image.width();
-		// 	//   var tempHeight = 285*imgRatio
-		// 	//   if ((285*imgRatio)>570){
-		// 	//   	image.batch()
-		// 	//   	.resize(570/imgRatio,570)
-		// 	//     .writeFile(__dirname +'/uploads/samples/'+filename, function(err){
-		// 	//     	console.log("resample done");
-		// 	//     });
-		// 	//   }
-		// 	//   else{
-		// 	//   	image.batch()
-		// 	//   	.resize(285,285*imgRatio)
-		// 	//     .writeFile(__dirname +'/uploads/samples/'+filename, function(err){
-		// 	//     	console.log("resample done");
-		// 	//     });
-		// 	//   }
-		// 	// });
-  //     res.json({
-  //       filename: filename
-  //     });
-  //   });
-  // });
+	sharp(__dirname+'/../../uploads/qqrequest/originals/'+req.file.filename).resize(400, null).toFile(__dirname+'/../../uploads/qqrequest/samples/'+req.file.filename, function(err) {
+         if (err) {
+           throw err;
+         }
+         res.json({filename: req.file.filename});
+    });
 })
 
 module.exports = router;
