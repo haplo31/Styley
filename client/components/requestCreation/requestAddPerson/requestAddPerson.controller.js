465941dc4d62efff84fa0f8046f07fa8c672f10d
drougard <drougardn@gmail.com>
2016-01-31 03:09:14 +0100

    slider example of addperson request

--- client/components/requestCreation/requestAddPerson/requestAddPerson.controller.js
+++ client/components/requestCreation/requestAddPerson/requestAddPerson.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('styleyApp')
-  .controller('addPersCtrl', function ($scope) {
+  .controller('addPersCtrl', function ($scope,$timeout,$http,Auth,Upload,$modalInstance) {
     $scope.message = 'Hello';
+    $scope.isLoggedIn = Auth.isLoggedIn
+		console.log(Auth.isLoggedIn())
+    $scope.step=1;
+    $scope.completedStep=1;
+	$scope.sliderSrc1 = '../../../assets/images/remperson3.png';
+  	$scope.sliderSrc2 = '../../../assets/images/remperson2.png';	
+  	$scope.backgroundImg = '../../../assets/images/remperson1.png';	
+  	$timeout(function(){$('.slider').slider(); }, 500);
   });
